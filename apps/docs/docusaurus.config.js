acf03cf284427401b0e9f73063f91a17bda4f65e
Ludvík Prokopec <39367469+lewis-wow@users.noreply.github.com>
2023-10-07 20:37:41 +0200

    [docs] Docusaurus (#4)
    * fix docs
    
    * docs fixes

---
+++ apps/docs/docusaurus.config.js
@@ -0,0 +1,75 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/palenight');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Schema-rpc',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://schema-rpc-docs.vercel.app/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'lewis-wow', // Usually your GitHub org/user name.
+  projectName: 'Schema-rpc', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/lewis-wow/bakalarska-prace/',
+          routeBasePath: '/', // Serve the blog at the site's root
+          /* other blog options */
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      navbar: {
+        title: 'Schema-rpc',
+        items: [
+          {
+            href: 'https://github.com/lewis-wow/bakalarska-prace',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
