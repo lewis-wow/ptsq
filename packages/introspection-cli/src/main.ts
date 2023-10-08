5a7d655f4672277e7d423742074f309d6aac55b1
Ludvík Prokopec <prokolud@fit.cvut.cz>
2023-10-08 20:37:08 +0200

    init cli

---
+++ packages/introspection-cli/src/main.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import yargs from 'yargs';
+import { compile } from 'json-schema-to-typescript';
+import { writeFile } from 'fs/promises';
+import { resolve } from 'path';
+
+yargs.scriptName('introspection-cli');
+
+yargs.usage('\nUsage: $0 [cmd] <args>').alias('h', 'help');
+
+yargs.command(
+  'url <url>',
+  'Add schema url',
+  (yargs) =>
+    yargs.positional('url', {
+      type: 'string',
+      describe: 'the schema url to introspectate',
+      demandOption: true,
+    }),
+  async (argv) => {
+    const jsonSchema = await axios.get(argv.url);
+    const tsSchema = await compile(jsonSchema, 'MySchema', {
+      unreachableDefinitions: true,
+    });
+    await writeFile('schema.ts', tsSchema, 'utf8');
+
+    console.log(`Schema generated into ${resolve('./schema.ts')}`);
+  }
+);
+
+yargs.parse();
