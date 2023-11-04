99b59075917d0ac6262713311d03681750109d7c
Ludvík Prokopec <prokolud@fit.cvut.cz>
2023-11-04 21:07:08 +0100

    fix build

--- examples/express/src/server.ts
+++ examples/express/src/server.ts
@@ -1,4 +1,4 @@
-import { createServer, expressAdapter, ExpressAdapterContext, HTTPError } from '@ptsq/server';
+import { createServer, expressAdapter, ExpressAdapterContext } from '@ptsq/server';
 import express from 'express';
 import { z } from 'zod';
 
@@ -10,32 +10,6 @@ const { router, resolver, serve } = createServer({
     res,
     user: 'user' as 'user' | undefined | null,
   }),
-});
-
-const anotherResolver = resolver.use(async ({ ctx, next }) => {
-  if (ctx.user === null) throw new HTTPError({ code: 'CONFLICT' });
-
-  console.time('start');
-
-  const res = await next({
-    user: ctx.user,
-  });
-
-  console.timeEnd('start');
-
-  return res;
-});
-
-const anotherResolverW = anotherResolver.use(({ ctx, next }) => {
-  if (ctx.user === undefined) throw new HTTPError({ code: 'CONFLICT' });
-
-  const res = next({
-    user: ctx.user,
-  });
-
-  console.log('asdf');
-
-  return res;
 });
 
 const greetingsQuery = resolver.query({
