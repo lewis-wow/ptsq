4c4d4cc1780a737398a7bba233dd05ad6befa6a4
Ludvík Prokopec <prokolud@fit.cvut.cz>
2023-12-06 23:17:25 +0100

    update

--- examples/next/src/pages/index.tsx
+++ examples/next/src/pages/index.tsx
@@ -1,21 +1,49 @@
 import { client } from '@/client';
-import { useMutation } from '@tanstack/react-query';
-import { useEffect } from 'react';
+import { TextField } from '@/components/TextField';
+import { createUserSchema } from '@/server/validation';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { Formik } from 'formik';
+import { toFormikValidate } from 'zod-formik-adapter';
 
 export default function Index() {
-  const createUserMutation = useMutation({
-    mutationKey: [''],
-    mutationFn: (params) => client.user.create(params),
+  const createUserMutation = useMutation<
+    Awaited<ReturnType<typeof client.user.create.mutate>>,
+    Error,
+    Parameters<typeof client.user.create.mutate>[0]
+  >({
+    mutationKey: ['users'],
+    mutationFn: (params) => client.user.create.mutate(params),
   });
 
-  useEffect(() => {
-    client.user.create
-      .mutate({
-        name: 'Johnatan',
-        email: 'kokoss@boss.com',
-      })
-      .then(console.log);
-  }, []);
+  const usersQuery = useQuery({
+    queryKey: ['users'],
+    queryFn: (params) => client.user.create.mutate(params),
+  });
 
-  return <main></main>;
+  return (
+    <main>
+      <h2>Vytvořit uživatele</h2>
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+        }}
+        validate={toFormikValidate(createUserSchema)}
+        onSubmit={(values) => {
+          createUserMutation.mutate(values);
+        }}
+      >
+        {({ isSubmitting, isValid, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <TextField name="name" />
+            <TextField name="email" type="email" />
+
+            <button type="submit" disabled={isSubmitting || !isValid}>
+              Create user
+            </button>
+          </form>
+        )}
+      </Formik>
+    </main>
+  );
 }
