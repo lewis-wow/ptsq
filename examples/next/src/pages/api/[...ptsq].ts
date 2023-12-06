ef665bc79661f25b4656a8d8df9fd36bce6ae5ea
Ludvík Prokopec <prokolud@fit.cvut.cz>
2023-12-06 22:06:06 +0100

    fixes

--- examples/next/src/pages/api/[...ptsq].ts
+++ examples/next/src/pages/api/[...ptsq].ts
@@ -1,5 +1,6 @@
 import { serve } from '@/server/ptsq';
 import { baseRouter } from '@/server/routes/root';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export const config = {
   api: {
--- examples/next/src/pages/index.tsx
+++ examples/next/src/pages/index.tsx
@@ -1,7 +1,13 @@
 import { client } from '@/client';
+import { useMutation } from '@tanstack/react-query';
 import { useEffect } from 'react';
 
-export default function Home() {
+export default function Index() {
+  const createUserMutation = useMutation({
+    mutationKey: [''],
+    mutationFn: (params) => client.user.create(params),
+  });
+
   useEffect(() => {
     client.user.create
       .mutate({
