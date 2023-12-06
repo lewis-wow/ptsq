98de5ddeb609368e581162985f3c20ad720336cb
Ludvík Prokopec <39367469+lewis-wow@users.noreply.github.com>
2023-12-06 23:43:59 +0100

    [example] fix build (#68)

--- examples/next/src/pages/api/[...ptsq].ts
+++ examples/next/src/pages/api/[...ptsq].ts
@@ -1,6 +1,5 @@
 import { serve } from '@/server/ptsq';
 import { baseRouter } from '@/server/routes/root';
-import { NextApiRequest, NextApiResponse } from 'next';
 
 export const config = {
   api: {
