c3acd74a99514995cdff9bbdaedbbd62122aa9a2
Ludvík Prokopec <prokolud@fit.cvut.cz>
2024-01-15 23:37:11 +0100

    try istanbul

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'v8',
+      provider: 'istanbul',
       include: [
         'packages/client/src/**',
         'packages/react-client/src/**',
