3c77839eea6c947cfe5e6598377f6310a5a07362
Ludvík Prokopec <prokolud@fit.cvut.cz>
2024-01-27 20:48:10 +0100

    add basic example

---
+++ examples/basic/src/server.ts
@@ -0,0 +1,26 @@
+import { createServer } from 'http';
+import { PtsqServer } from '@ptsq/server';
+import { Type } from '@sinclair/typebox';
+
+const { resolver, router, serve } = PtsqServer.init().create();
+
+const greetingsQuery = resolver
+  .args(
+    Type.Object({
+      name: Type.String(),
+    }),
+  )
+  .output(Type.TemplateLiteral('Hello, ${string}!'))
+  .query(({ input }) => `Hello, ${input.name}!`);
+
+const baseRouter = router({
+  greetings: greetingsQuery,
+});
+
+const server = createServer(serve(baseRouter));
+
+server.listen(4000, () => {
+  console.log(`PTSQ server running on http://localhost:4000/ptsq`);
+});
+
+export type BaseRouter = typeof baseRouter;
