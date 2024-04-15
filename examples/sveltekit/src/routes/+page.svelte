339e92c2f4cf1707e4fcf52060ee8e4374c561e3
Ludvík Prokopec <39367469+lewis-wow@users.noreply.github.com>
2024-04-15 16:06:06 +0200

    [examples] sveltekit example (#159)

---
+++ examples/sveltekit/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { client } from '$lib/ptsq';
+
+  const greetingsQuery = client.greetings.createQuery();
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<div>{JSON.stringify($greetingsQuery)}</div>
